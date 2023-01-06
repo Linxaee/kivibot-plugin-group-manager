20b742749450cf377c9c34d7efcf34cc8f706a05
Linxae <702611663@qq.com>
2023-01-06 12:30:43 +0800

    fix: 修复头衔为空时没有清空头衔

--- src/module/title/selfTitleHandler.ts
+++ src/module/title/selfTitleHandler.ts
@@ -9,13 +9,17 @@ export const selfTitleHandler: GroupEventHandler = async (e, plugin, config, arg
     // 是否开启自行申请
     const { enableSelf } = config.titleConfig as ModuleConfig;
     // bot若不是管理或群主则发送
-    if (!roleAuth.selfIsGroupOwner(group)) return e.reply(selfNoAuthMsg + "设置头衔需将bot设置为群主");
+    if (!roleAuth.selfIsGroupOwner(group)) return e.reply(selfNoAuthMsg + "设置头衔需将bot设置为群主哦");
     // 判断是否开启自己申请
     if (!enableSelf) return e.reply("本群尚未开启自行申请头衔功能,可以联系bot管理员开启该功能", true);
     else {
         // 头衔
         const title = argMsg.split(" ")[0];
         if (!validateTitle(title)) return e.reply(`头衔大于12个字符,请重新设置`);
+        if (!title) {
+            group.setTitle(sender_id);
+            return e.reply(`已清空您的头衔`, true);
+        }
         group.setTitle(sender_id, title);
         return e.reply(`已将您的头衔设置为 ${title}`, true);
     }
