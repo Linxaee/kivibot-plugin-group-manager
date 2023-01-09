99aabb9c2cbb92cc06f3f0f14b68c9f12cb18c6a
Linxae <702611663@qq.com>
2023-01-09 13:38:32 +0800

    fix: 修改deepMerge逻辑保证groupConfigs为最新的对象形式

--- src/utils/deepMerge.ts
+++ src/utils/deepMerge.ts
@@ -7,10 +7,19 @@
 export const deepMerge = (target: any, source: any) => {
     for (const key of Object.keys(source)) {
         if (source[key] instanceof Object && !Array.isArray(source[key])) {
+            // 若目标对象中没有源对象的当前key,则向目标对象添加该key,并将空对象和源对象的value再进行递归合并
             if (!(key in target)) {
                 Object.assign(target, { [key]: {} });
             }
             deepMerge(target[key], source[key]);
+            // 若groupConfigs还是数组则更新为对象形式
+        } else if (Array.isArray(source[key]) && key === "groupConfigs") {
+            const newObj = source[key].reduce((total: any, value: any) => {
+                const { gid, ...exceptGid } = value;
+                total[gid] = exceptGid;
+                return total;
+            }, {});
+            Object.assign(target[key], newObj);
         } else {
             Object.assign(target, { [key]: source[key] });
         }
